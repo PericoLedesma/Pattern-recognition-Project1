9bca847145e115527a2f131ff5b449d7558e2003
Pietro Ledesma <67062845+PericoLedesma@users.noreply.github.com>
2021-11-25 23:36:28 +0100

    Task 1 finished

--- Task 1/Task_1.m
+++ Task 1/Task_1.m
@@ -1,17 +1,16 @@
 % Task 1: Pairwise correlation coeﬀicients
-
 clear all
 clc
 
 % Firts element Height, Second element Age, Third element Weight(kg)
+
+%Load file
 array = importdata('Assignment1-files/Task_1/task_1.mat');
 
 size_array = size(array);
 
-
 number_rows = size_array(1, 1);
-
-number_colums = size_array(1, 2);
+number_columns = size_array(1, 2);
 
 
 
@@ -30,85 +29,199 @@ number_colums = size_array(1, 2);
 % %Delete until here
 
 
+
+%Display of all the features (1vs1)
+
+figure;
+
+for i = 1: number_columns
+
+    %Columns 
+    column1 = i;
+    column2 = column1 +1;
+    
+    if column2 > 3
+        column2 = 1;
+    end 
+
+    %Plot graph
+    subplot(3,1,i);
+    scatter(array(:,column1), array(:,column2),'x', 'red', 'LineWidth',2);
+    xlim([(min(array(:,column1)) - 1) (max(array(:,column1)) + 1)])
+    ylim([(min(array(:,column2)) - 1) (max(array(:,column2)) + 1)])
+
+    %Title of axis
+    label_column1 = 'Height(cm)';
+    label_column2 = 'Age(years)';
+    label_column3 = 'Weight(Kg)';
+
+    %Axis labels X
+    if column1 == 1
+        xlabel(label_column1)
+    end 
+    if column1 == 2
+        xlabel(label_column2)
+    end 
+    if column1 == 3
+        xlabel(label_column3)
+    end 
+
+    %Axis labels Y
+    if column2 == 1
+        ylabel(label_column1)
+    end 
+    if column2 == 2
+        ylabel(label_column2)
+    end 
+    if column2 == 3
+        ylabel(label_column3)
+    end 
+
+
+    title('Array display')
+        
+ end
+
+
 %  We calculate the mean of the features
+ avg = mean(array);
 
-sum_colum1 = 0;
-sum_colum2 = 0;
-sum_colum3 = 0;
+%Correlation
+C = cov(array);
 
-for i = 1:number_rows
+correlation = [0; 0; 0];
+
+for i = 1: number_columns
+
+    %Columns 
+    column1 = i;
+    column2 = column1 +1;
     
-    sum_colum1 = sum_colum1 + array(i, 1);
-    sum_colum2 = sum_colum2 + array(i, 2);
-    sum_colum3 = sum_colum3 + array(i, 3);
+    if column2 > 3
+        column2 = 1;
+    end 
     
+    a = C(column1, column1)*C(column2, column2);
+    b = C(column1, column2)/sqrtm(a);
+    c = [b; column1; column2];
+    
+    correlation = [correlation c];   
+    
+    %fprintf('The correlation between the column %d and column %d is: %f\n',column1,column2, correlation);
+end 
+   
+
+
+
+%Max value 
+  
+correlation = abs(correlation);
+correlation1 = max(correlation(1,:));
+
+%Display the two features for which the correlation coeﬀicient is largest
+
+for i = 1:length(correlation)
+    if correlation(1,i) == correlation1
+        column1 = correlation(2,i);
+        column2 = correlation(3,i);
+    end 
 end 
 
-avg1 = sum_colum1 / number_rows;
-avg2 = sum_colum2 / number_rows;
-avg3 = sum_colum3 / number_rows;
 
 
+%Title of column
+if column1 == 1
+    column_name1 = label_column1;
+end 
+if column1 == 2
+    column_name1 = label_column2;
+end 
+if column1 == 3
+    column_name1 = label_column3;
+end 
+
+if column2 == 1
+    column_name2 = label_column1;
+end 
+if column2 == 2
+    column_name2 = (label_column2);
+end 
+if column2 == 3
+    column_name2 = (label_column3);
+end 
+
+fprintf('The two features for which the correlation coeﬀicient is largest are the feature %s(column %d) and the feature %s(column %d).\nThe coeficient is %f\n', column_name1,column1, column_name2,column2,  correlation1);
+
+%Display of the plot A --> Max correlation
 
 
-% % Correlation of colum 1 vs 2 
-% 
-% %Correlation formula 1 = [Sumatory (xi-meanx)(yi-meany))] / [Sumatory((xi-meanx)^2)* Sumatory(yi-meany)^2)]1^/2
-% 
-% sum_top = 0;
-% sum_bot1 = 0;
-% sum_bot2 = 0;
-% 
-% for i =1:number_rows
-%     
-%     %Top
-%     sum_top = sum_top + (array(i,1) - avg1)*(array(i,2)-avg2);
-%     
-%     %Bot
-%     sum_bot1 = sum_bot1 + (array(i,1) - avg1)^2;
-%     sum_bot2 = sum_bot2 + (array(i,2) - avg2)^2;
-%     
-% end 
-% 
-% correlation1 = sum_top/sqrt((sum_bot1 * sum_bot2));
-% 
-% fprintf('Value by formula 1: %d \n', correlation1);
+%Plot graph
+figure;
+subplot(2,1,1);
+scatter(array(:,column1), array(:,column2),'x', 'red', 'LineWidth',2);
+xlim([(min(array(:,column1)) - 1) (max(array(:,column1)) + 1)])
+ylim([(min(array(:,column2)) - 1) (max(array(:,column2)) + 1)])
+
+xlabel(column_name1)
+ylabel(column_name2)
+
+title('Plot A')
 
 
-for colum1 =1:(number_colums)
-    
-   colum2 = colum1 + 1;
-   
-    if colum2 > 3
-        colum2 = 1;
+%Second largest correlation 
+
+for i =1:length(correlation)
+    if correlation(1,i)== correlation1
+        correlation(1,i) = 0;
+        i = length(correlation);
     end 
-    
-    %Correlation formula 2 
-
-    sum_top = 0;
-    sum_bot1 = 0;
-    sum_bot2 = 0;
-
-    for i =1:number_rows
-
-        %Top
-        sum_top = sum_top + array(i,colum1) * array(i,colum2);
-
-        %Bot
-        sum_bot1 = sum_bot1 + array(i,1)^2;
-        sum_bot2 = sum_bot2 + array(i,2)^2;
-
-    end 
-
-    a = sqrt(sum_bot1 - number_rows * avg1^2);
-    b = sqrt(sum_bot2 - number_rows * avg2^2);
-    
-    correlation2 = [sum_top - number_rows * avg1 * avg2] /(a * b);
-    
-    fprintf('Correlation between column %d and column %d: %d\n', colum1, colum2, correlation2);
-
 end
 
+correlation2 = max(correlation(1,:));
+
+%Display the two features for which the correlation coeﬀicient is largest
+
+for i = 1:length(correlation)
+    if correlation(1,i) == correlation2
+        column1 = correlation(2,i);
+        column2 = correlation(3,i);
+    end 
+end 
 
 
+%Display of the plot B --> Second higher correlation
 
+%Title of column
+if column1 == 1
+    column_name1 = label_column1;
+end 
+if column1 == 2
+    column_name1 = label_column2;
+end 
+if column1 == 3
+    column_name1 = label_column3;
+end 
+
+if column2 == 1
+    column_name2 = label_column1;
+end 
+if column2 == 2
+    column_name2 = (label_column2);
+end 
+if column2 == 3
+    column_name2 = (label_column3);
+end 
+
+fprintf('The the two features for which the second largest correlation coeﬀicient are the feature %s(column %d) and the feature %s(column %d).\n The coeficient is: %f\n', column_name1,column1, column_name2,column2,  correlation2);
+
+%Plot graph
+%figure;
+subplot(2,1,2);
+scatter(array(:,column1), array(:,column2),'x', 'red', 'LineWidth',2);
+xlim([(min(array(:,column1)) - 1) (max(array(:,column1)) + 1)])
+ylim([(min(array(:,column2)) - 1) (max(array(:,column2)) + 1)])
+
+xlabel(column_name1)
+ylabel(column_name2)
+
+title('Plot B')
