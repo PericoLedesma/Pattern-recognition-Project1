1d823dbb0fb3a79345e9eedd69d6bb83efb107e8
Bonne Jongsma <b.v.jongsma@student.rug.nl>
2021-11-26 14:45:14 +0100

    finished task 3

--- Task 3/task_03.m
+++ Task 3/task_03.m
@@ -7,7 +7,7 @@ f5 = [4 6 5];
 F = [f1; f2; f3; f4; f5]
 
 %calculate the mean
-mu =0.2*[sum(F(:,1)), sum(F(:,2)), sum(F(:,3))];
+mu =0.2*[sum(F(:,1)), sum(F(:,2)), sum(F(:,3))]
 
 %covariance
 S = eye(3);
@@ -18,4 +18,13 @@ for i = 1:3
       S(i,j) =  (1/4)*sum(xi.*yi);
    end
 end
+fprintf 'S = \n'
 disp(S)
+
+%%cal the probability for the vectors
+x1 = [5 5 6]';
+x2 = [3 5 7]';
+x3 = [4 6.5 1]';
+p1 = my_mnd(x1,mu',S,3)
+p2 = my_mnd(x2,mu',S,3)
+p3 = my_mnd(x3,mu',S,3)
