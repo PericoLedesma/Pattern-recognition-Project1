e7d3316b4b23984573c6d9d1f39b1c44076791c3
Pietro Ledesma <67062845+PericoLedesma@users.noreply.github.com>
2021-12-03 13:10:00 +0100

    TASK 9

--- Task 9/task_9.m
+++ Task 9/task_9.m
@@ -11,21 +11,17 @@ data_structure = load('task_9_outcomes.mat');
 
 % Parameters
 
-x_criterion = 5;
+x_criterion = 0;
 
-mean = [5 15];
+mean = [2 10];
 variance = [4 4];
 
 
 x1 = -10;
-x2 = 25;
+x2 = 30;
 
 x = [x1:0.1:x2];
 
-% %Interval μ1 − 3σ
-% 
-% lower_lim = mean(1) - 3 * variance(1);
-% up_limit = mean(2) + 3 * variance(2);
 
 % Normal distributions (PDFs)
 
@@ -49,7 +45,9 @@ for i =1:length(pdf_normal(1,:))
     intersection_point_x = intersection_point_x + 0.1;
 end
 
-x_criterion = intersection_point_x;
+if x_criterion ~= 0
+    x_criterion = intersection_point_x;
+end
 
 % Cumulative distribution function (CDFs)
 
@@ -64,54 +62,59 @@ f.Position = [100 100 800 800];
 
 subplot(3,1,1);
 
-plot(x,pdf_normal(1,:),'LineWidth',2)
+plot(x,pdf_normal(1,:),'LineWidth',2,'DisplayName','N(μ=7)')
 
 hold on
 
-plot(x,pdf_normal(2,:),'LineWidth',2)
+plot(x,pdf_normal(2,:),'LineWidth',2,'DisplayName','N(μ=10)')
 
 xlim([x1 x2])
 ylim([0 0.15])
 
-xline(x_criterion,'--');
+xline(x_criterion,'--','DisplayName','Threshold');
 
 title('Normal density function')
+
+% legend('N(mean = 7', 'Intersection point')
 
 hold on
 
 % Display intersection point
 
-plot(intersection_point_x,intersection_point_y,'redo', 'MarkerSize',20)  
+plot(intersection_point_x,intersection_point_y,'redo', 'MarkerSize',20,'DisplayName','Intersection')  
 
+legend
 hold on
 
 % Display CDF
 
 subplot(3,1,2);
-plot(x,cdf_normal(1,:),'LineWidth',2)
+plot(x,cdf_normal(1,:),'LineWidth',2,'DisplayName','N(μ=7)')
 
 hold on
 
-plot(x,cdf_normal(2,:),'LineWidth',2)
+plot(x,cdf_normal(2,:),'LineWidth',2,'DisplayName','N(μ=10)')
 
 xlim([x1 x2])
 ylim([0 2])
 
-xline(x_criterion,'--');
+xline(x_criterion,'--','DisplayName','Threshold');
 
 
 title('Cumulative distribution function')
+legend
 
 hold on
 
 
 
-%% Calculation probability between two points
+%% Calculation probability
 
 low_point = x_criterion;
-up_point = 100;
+up_point = 1000; % We should use inf number, but for this exercise that we dont need accurate results I just use a big number
 
 points = [low_point up_point];
+
 
 % Area of the first normal distribution (false alarm (fa))
 
@@ -121,11 +124,14 @@ probability(1,:) = normcdf(points,mean(1),variance(1));
 
 probability_false_alarm =  probability(1,2) - probability(1,1);
 
+
 % Area of the second normal distribution (hit (fa))
 
 probability(2,:) = normcdf(points,mean(2),variance(2));
 
 probability_hit = probability(2,2) - probability(2,1) - probability_false_alarm;
+
+
 
 % Display probabilities
 
@@ -138,6 +144,8 @@ y = [probability_hit probability_false_alarm];
 
 bar(X, y)
 
+ylim([0 1]);
+
 %  xlim([-10 51])
 %  ylim([0 1])
 
@@ -146,3 +154,18 @@ title('Probabilities')
 hold on
 
 disp(probability);
+
+
+%% Second part: ROC
+
+% cdf_normal(1,:) 
+% 
+% 
+% x1 = -10;
+% x2 = 25;
+% 
+% x = [x1:0.1:x2];
+% 
+% cdf_normal(1,:) = cdf(pd(1),x);
+% cdf_normal(2,:) = cdf(pd(2),x);
+% 
