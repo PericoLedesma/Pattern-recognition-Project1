abf6a6cd1c40f0555afb68874d20c6d40bce2b57
Sarandis Doulgeris <s.doulgeris@student.rug.nl>
2021-12-15 20:54:00 +0100

    modified

--- Task 6/entropy.m
+++ Task 6/entropy.m
@@ -1,10 +1,7 @@
 function ent_val = entropy(dataset)
-
 ent_val = 0;
-
 for i=unique(dataset(:,end))'
     ent_val = ent_val -(sum(dataset(:)== i)/size(dataset,1))...
               * log2(sum(dataset(:)== i )/size(dataset,1));
 end
-
 end
--- Task 6/information_gain.m
+++ Task 6/information_gain.m
@@ -1,15 +1,11 @@
 function [gain] = information_gain(entropy, subsets, S, entropy_v)
-
     for j=1:size(entropy_v, 1)
         temp = 0;
-        for i=1:size(subsets, 1)
-    
+        for i=1:size(subsets, 1)    
             S_v = length(subsets{j, i}(:,1));
-            temp = temp + (S_v/S) * entropy_v(j, i);
-              
+            temp = temp + (S_v/S) * entropy_v(j, i);              
         end
         gain{j} = entropy - temp;
     end
-
 end
 
